e9b41c19794020be1478362b7b97ebc345ecb401
JonasVianaSales <jonas.sales@sou.inteli.edu.br>
2022-03-13 14:16:47 -0300

    Layout Level01 e Hub adicionados

---
+++ src/DeIV/Global.gd
@@ -0,0 +1,3 @@
+extends Node
+
+var hardware = 0
--- src/DeIV/src/Actors/Player.gd
+++ src/DeIV/src/Actors/Player.gd
@@ -1,54 +1,61 @@
 extends KinematicBody2D
 
-const gravity:float = 1700.0
-const speed: = Vector2(800.0, 800.0)
+const GRAVITY:float = 1700.0
+const SPEED: = Vector2(800.0, 800.0)
 var _velocity: = Vector2.ZERO
+
 
 func _physics_process(_delta: float) -> void:
 	match get_tree().current_scene.name:
-		"Hub":
-			var is_jump_interrupted:bool = Input.is_action_just_released("jump") and _velocity.y < 0.0
-			var direction: = get_physics_direction()
-			_velocity = calculate_physics_move_velocity(direction, speed, is_jump_interrupted)
-			_velocity = move_and_slide(_velocity, Vector2.UP)
 		"Level01":
-			var direction: = get_non_physics_direction()
-			_velocity = calculate_non_physics_move_velocity(direction, speed)
+			var _is_jump_interrupted:bool = Input.is_action_just_released("jump") and _velocity.y < 0.0
+			var _direction: = get_physics_direction()
+			_velocity = calculate_physics_move_velocity(_direction, SPEED, _is_jump_interrupted)
+			_velocity = move_and_slide(_velocity, Vector2.UP)
+		"Hub":
+			var _direction: = get_non_physics_direction()
+			_velocity = calculate_non_physics_move_velocity(_direction, SPEED)
 			_velocity = move_and_slide(_velocity)
+			
 
 func get_physics_direction() -> Vector2:
 	return Vector2(Input.get_action_strength("move_right") - Input.get_action_strength("move_left"),
 	-1.0 if Input.is_action_just_pressed("jump") and is_on_floor() else 1.0)
 	
+	
 func get_non_physics_direction() -> Vector2:
 	return Vector2(Input.get_action_strength("move_right") - Input.get_action_strength("move_left"),
 	 Input.get_action_strength("move_down") - Input.get_action_strength("jump"))
+	
 
 func calculate_physics_move_velocity(
-		direction,
-		speed,
-		is_jump_interrupted
+		_direction,
+		SPEED,
+		_is_jump_interrupted
 	) -> Vector2:
-	var out = _velocity
-	out.x = speed.x * direction.x
-	out.y += gravity * get_physics_process_delta_time()
-	if direction.y == -1.0:
-		out.y = speed.y * direction.y
-	if is_jump_interrupted:
-		out.y = 0.0
-	return out
+	var _out = _velocity
+	_out.x = SPEED.x * _direction.x
+	_out.y += GRAVITY * get_physics_process_delta_time()
+	if _direction.y == -1.0:
+		_out.y = SPEED.y * _direction.y
+	if _is_jump_interrupted:
+		_out.y = 0.0
+	return _out
+
 
 func calculate_non_physics_move_velocity(
-		direction,
-		speed
+		_direction,
+		SPEED
 	) -> Vector2:
-	var out = _velocity.normalized()
-	out.x = speed.x * direction.x
-	out.y = speed.y * direction.y
-	return out
+	var _out = _velocity.normalized()
+	_out.x = SPEED.x * _direction.x
+	_out.y = SPEED.y * _direction.y
+	return _out
+
 
 func _ready():
 	OS.window_fullscreen = true # Deixa o jogo em tela cheia
+
 
 # Termina o jogo caso ESC seja pressionado
 func _input(_event):
